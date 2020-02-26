4396721150a2a114c54571bbeb6e27be471026b9
indupriya98 <imindu98@gmail.com>
2020-02-26 23:26:20 +0530

    task5 committed

---
+++ log4j_maven/src/main/java/epam_task5/log4j_maven/App.java
@@ -0,0 +1,13 @@
+package epam_task5.log4j_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ log4j_maven/src/test/java/epam_task5/log4j_maven/AppTest.java
@@ -0,0 +1,20 @@
+package epam_task5.log4j_maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
